a09de410bcf2633f35848de3cb4061401ed6b661
anant <anantdevdubey@hotmail.com>
2018-10-22 12:59:27 +0530

    UI nearly done

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const sounds = [
   'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3',
 ]
 
-let gameState = 'OFF';
+let gameState = 'ON';
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.restart').click(gameStart);
-  $('#power').click('gameStart');
+  $('#power').click(gameStart);
+  $('#power').click(() => {
+    console.log('clicked');
+    $('.flip').toggleClass('switch-on');
+  });
+  $('.round-btn').click(function(){ 
+    console.log((this)); 
+    $(this).toggleClass('pressed-on'); 
+  });
 });
 
-function gameStart(){
-  
-}
-if(gameState=='ON'){
-  $('.btn--red').click(()=>{playSound(sounds[0])})
+function gameStart() {
+  if (gameState == 'ON') {
+    $('.btn--red').click(() => { playSound(sounds[0]) })
 
+  }
 }
 
-function playSound(source){
+function playSound(source) {
   var flush = new Audio(source);
   flush.play();
   console.log('played')
